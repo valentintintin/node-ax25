f2806dd6659a57922dd42de164cb8f8a1f84eb0c
Valentin Saugnier <valentin.s.10@gmail.com>
2020-02-27 00:03:34 +0100

    [Fix] Missing index import

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ exports.kissDefs	= require("./kissDefs.js");
 exports.Defs		= require("./Defs.js");
 exports.Utils		= require("./Utils.js");
 exports.kissTNC		= require("./kissTNC.js");
+exports.kissTNCTcp		= require("./kissTNCTcp.js");
 exports.Packet		= require("./Packet.js");
 exports.Session		= require("./Session.js");
